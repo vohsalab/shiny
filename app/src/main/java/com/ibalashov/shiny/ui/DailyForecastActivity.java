9a7a6566135582a56d15e8e248bf740bf9e32eda
Ivan Balashov <sputnic1985@gmail.com>
2015-07-05 14:40:13 -0400

    started implementing list adapters  for the daily forecast

---
+++ app/src/main/java/com/ibalashov/shiny/ui/DailyForecastActivity.java
@@ -0,0 +1,25 @@
+package com.ibalashov.shiny.ui;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.ibalashov.shiny.R;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+
+        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+                "Saturday" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter);
+
+
+    }
+
+   }
--- app/src/main/java/com/ibalashov/shiny/ui/MainActivity.java
+++ app/src/main/java/com/ibalashov/shiny/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ibalashov.shiny.ui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -32,6 +33,7 @@ import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -234,5 +236,11 @@ public class MainActivity extends AppCompatActivity {
     private void alertUserAboutError() {
         AlertDialogFragment dialog = new AlertDialogFragment();
         dialog.show(getFragmentManager(), "error_dialog");
+    }
+
+    @OnClick (R.id.dailyButton)
+    public void  startDailyActivity(View view) {
+        Intent intent = new Intent(this, DailyForecastActivity.class);
+        startActivity(intent);
     }
 }
